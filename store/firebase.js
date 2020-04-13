d1c22fc98e3ce5186f66d19be54cb4ea67b69878
YAKA <hnudka@gmail.com>
2020-04-13 16:12:02 +0700

    Create env

--- store/firebase.js
+++ store/firebase.js
@@ -1,6 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-const config = {}
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STOREAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
 const firebaseApp = !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
 const db = firebaseApp.firestore();
